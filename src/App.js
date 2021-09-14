83ec7ce97350cbb188aea79037209aeefe02472c
luisluft <luberluft@gmail.com>
2021-09-14 09:02:33 -0400

    implement react icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Review from "./Review";
+import { FaFly } from "react-icons/fa";
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <h2>
+      reviews project setup
+      <FaFly className="icon" />
+    </h2>
+  );
 }
 
 export default App;
